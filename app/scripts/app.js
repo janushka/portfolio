fa1e17a7c220117cb07a11c5f514d8f52da87137
janushka <marc.njoku@icloud.com>
2014-07-16 16:42:09 +0200

    - added Karma libraries - workshop formular started

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,23 @@
 'use strict';
 
 angular
-  .module('portfolioApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/workshop', {
-        templateUrl: 'views/workshop.html'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('portfolioApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/workshop', {
+                templateUrl: 'views/workshop.html',
+                controller: 'WorkshopCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
