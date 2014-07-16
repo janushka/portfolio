9eae8e55c6abba7784957cd5e1f4110db6b216cf
Marc Njoku <marc.njoku@icloud.com>
2014-07-16 06:43:19 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular
+  .module('portfolioApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/workshop', {
+        templateUrl: 'views/workshop.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
