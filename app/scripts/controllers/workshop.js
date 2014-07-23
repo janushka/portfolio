5b415c03fa4f0eb857e0a61827b0749455e3f364
janushka <marc.njoku@icloud.com>
2014-07-23 14:18:48 +0200

    Added a function calculateScore() in workshop-Controller

--- app/scripts/controllers/workshop.js
+++ app/scripts/controllers/workshop.js
@@ -5,9 +5,21 @@ angular.module('portfolioApp')
         $scope.myName = 'Marc Njoku';
         $scope.buttonAus = 'workshop';
 
+        $scope.count = 0;
+
         $scope.evaluationButtonisDisabled = true;
-        $scope.enableEvaluationButton = function() {
+        $scope.enableEvaluationButton = function (rbName) {
             $scope.evaluationButtonisDisabled = false;
+
+            $scope.$watch(rbName, function(newValue, oldValue) {
+                $scope.count = $scope.count + 1;
+            });
+
             console.log('Der Button müsste aktiviert sein!');
+        };
+
+        $scope.calculateScore = function () {
+
+            console.log('Der Wert der Variable \'count\' = ' + $scope.count);
         };
     });
